32aa30fb077a66725588a955875ee001d809de24
Brijesh-09 <brijeshkori22@gmail.com>
2026-09-11 15:55:48 +0530

    update enc

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -56,7 +56,7 @@ export default function ContactPage({
       // Web3Forms access key
       formData.append(
         'access_key',
-        import.meta.env.VITE_WEB3FORMS_ACCESS_KEY,
+        import.meta.env.FORMS_ACCESS_KEY,
       );
 
       // Email subject
